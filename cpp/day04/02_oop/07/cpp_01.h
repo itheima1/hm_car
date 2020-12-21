03e0cde30e59b1d35f198e9d3649963ad5a013ff
itheima1 <qinyongquan@itcast.cn>
2020-12-21 18:09:11 +0800

    删除无效代码

--- cpp/day04/02_oop/07/cpp_01.h
+++
@@ -1,8 +0,0 @@
-
-#ifndef DAY04_CPP_01_H
-#define DAY04_CPP_01_H
-#define LABEL
-#include "first.c"
-
-
-#endif //DAY04_CPP_01_H
--- cpp/day04/02_oop/07/first.c
+++
@@ -1,5 +0,0 @@
-#ifdef  LABEL
-#define extern
-#endif
-
-#define mylog(format,...) printf(format"\r\n",##__VA_ARGS__) extern
